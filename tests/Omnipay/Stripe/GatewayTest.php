e9bdad2265518aea019e71d48f802dde9ea814f8
Adrian Macneil <adrian@adrianmacneil.com>
2013-11-15 07:55:13 +1100

    Prepare for package separation

--- tests/Omnipay/Stripe/GatewayTest.php
+++ tests/Omnipay/Stripe/GatewayTest.php
@@ -2,7 +2,7 @@
 
 namespace Omnipay\Stripe;
 
-use Omnipay\GatewayTestCase;
+use Omnipay\Tests\GatewayTestCase;
 
 class GatewayTest extends GatewayTestCase
 {
--- tests/Omnipay/Stripe/Message/DeleteCardRequestTest.php
+++ tests/Omnipay/Stripe/Message/DeleteCardRequestTest.php
@@ -2,7 +2,7 @@
 
 namespace Omnipay\Stripe\Message;
 
-use Omnipay\TestCase;
+use Omnipay\Tests\TestCase;
 
 class DeleteCardRequestTest extends TestCase
 {
