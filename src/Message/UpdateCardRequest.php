72dd50fb9133f688d15687025f49cb1fec8ce570
Del <del@babel.com.au>
2015-02-13 12:34:31 +0700

    Updating API documentation

--- src/Message/UpdateCardRequest.php
+++ src/Message/UpdateCardRequest.php
@@ -1,9 +1,17 @@
 <?php
+/**
+ * Stripe Update Credit Card Request
+ */
 
 namespace Omnipay\Stripe\Message;
 
 /**
  * Stripe Update Credit Card Request
+ *
+ * This needs further work and/or explanation because it requires
+ * a customer ID.
+ *
+ * @link https://stripe.com/docs/api#update_card
  */
 class UpdateCardRequest extends AbstractRequest
 {
