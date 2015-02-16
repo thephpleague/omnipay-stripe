58665f881242d486cbcfeb7ee77e4b0cfe530787
Del <del@babel.com.au>
2015-02-16 16:28:12 +0700

    Adding extra code examples

--- src/Message/CreateCardRequest.php
+++ src/Message/CreateCardRequest.php
@@ -20,6 +20,42 @@ namespace Omnipay\Stripe\Message;
  * response in that case will then contain both a customer token
  * and a card token, and is essentially the same as CreateCustomerRequest
  *
+ * Example.  This example assumes that you have already created a
+ * customer, and that the customer reference is stored in $customer_id.
+ * See CreateCustomerRequest for the first part of this transaction.
+ *
+ * <code>
+ *   // Create a credit card object
+ *   // This card can be used for testing.
+ *   // The CreditCard object is also used for creating customers.
+ *   $new_card = new CreditCard(array(
+ *               'firstName'    => 'Example',
+ *               'lastName'     => 'Customer',
+ *               'number'       => '5555555555554444',
+ *               'expiryMonth'  => '01',
+ *               'expiryYear'   => '2020',
+ *               'cvv'          => '456',
+ *               'email'                 => 'customer@example.com',
+ *               'billingAddress1'       => '1 Lower Creek Road',
+ *               'billingCountry'        => 'AU',
+ *               'billingCity'           => 'Upper Swan',
+ *               'billingPostcode'       => '6999',
+ *               'billingState'          => 'WA',
+ *   ));
+ *
+ *   // Do a create card transaction on the gateway
+ *   $response = $gateway->createCard(array(
+ *       'card'              => $new_card,
+ *       'customerReference' => $customer_id,
+ *   ))->send();
+ *   if ($response->isSuccessful()) {
+ *       echo "Gateway createCard was successful.\n";
+ *       // Find the card ID
+ *       $card_id = $response->getCardReference();
+ *       echo "Card ID = " . $card_id . "\n";
+ *   }
+ * </code>
+ *
  * @see CreateCustomerRequest
  * @link https://stripe.com/docs/api#create_card
  */
--- src/Message/CreateCustomerRequest.php
+++ src/Message/CreateCustomerRequest.php
@@ -12,7 +12,52 @@ namespace Omnipay\Stripe\Message;
  * track multiple charges that are associated with the same customer.
  * The API allows you to create, delete, and update your customers.
  * You can retrieve individual customers as well as a list of all of
- * your customers. 
+ * your customers.
+ *
+ * Example:
+ *
+ * <code>
+ *   // Create a gateway for the Stripe Gateway
+ *   // (routes to GatewayFactory::create)
+ *   $gateway = Omnipay::create('Stripe');
+ *
+ *   // Initialise the gateway
+ *   $gateway->initialize(array(
+ *       'apiKey' => 'MyApiKey',
+ *   ));
+ *
+ *   // Create a credit card object
+ *   // This card can be used for testing.
+ *   // The CreditCard object is also used for creating customers.
+ *   $card = new CreditCard(array(
+ *               'firstName'    => 'Example',
+ *               'lastName'     => 'Customer',
+ *               'number'       => '4242424242424242',
+ *               'expiryMonth'  => '01',
+ *               'expiryYear'   => '2020',
+ *               'cvv'          => '123',
+ *               'email'                 => 'customer@example.com',
+ *               'billingAddress1'       => '1 Scrubby Creek Road',
+ *               'billingCountry'        => 'AU',
+ *               'billingCity'           => 'Scrubby Creek',
+ *               'billingPostcode'       => '4999',
+ *               'billingState'          => 'QLD',
+ *   ));
+ *
+ *   // Do a create customer transaction on the gateway
+ *   $response = $gateway->createCustomer(array(
+ *       'card'                     => $card,
+ *   ))->send();
+ *   if ($response->isSuccessful()) {
+ *       echo "Gateway createCustomer was successful.\n";
+ *       // Find the customer ID
+ *       $customer_id = $response->getCustomerReference();
+ *       echo "Customer ID = " . $customer_id . "\n";
+ *       // Find the card ID
+ *       $card_id = $response->getCardReference();
+ *       echo "Card ID = " . $card_id . "\n";
+ *   }
+ * </code>
  *
  * @link https://stripe.com/docs/api#customers
  */
