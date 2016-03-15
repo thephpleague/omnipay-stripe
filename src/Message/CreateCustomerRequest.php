f16b91bf7d48cacbee054430bc8656f50a5f17a6
Del <del@babel.com.au>
2016-03-15 11:53:56 +0800

    Clarify docblocks

--- src/Message/CreateCustomerRequest.php
+++ src/Message/CreateCustomerRequest.php
@@ -14,49 +14,69 @@ namespace Omnipay\Stripe\Message;
  * You can retrieve individual customers as well as a list of all of
  * your customers.
  *
- * Example:
+ * ### Examples
+ *
+ * #### Create Customer from Email Address
+ *
+ * This is the recommended way to create a customer object.
  *
  * <code>
- *   // Create a gateway for the Stripe Gateway
- *   // (routes to GatewayFactory::create)
- *   $gateway = Omnipay::create('Stripe');
+ * $response = $gateway->createCustomer(array(
+ *     'description'       => 'Test Customer',
+ *     'email'             => 'test123@example.com',
+ * ))->send();
+ * if ($response->isSuccessful()) {
+ *     echo "Gateway createCustomer was successful.\n";
+ *     // Find the card ID
+ *     $customer_id = $response->getCustomerReference();
+ *     echo "Customer ID = " . $customer_id . "\n";
+ * } else {
+ *     echo "Gateway createCustomer failed.\n";
+ *     echo "Error message == " . $response->getMessage() . "\n";
+ * }
+ * </code>
  *
- *   // Initialise the gateway
- *   $gateway->initialize(array(
- *       'apiKey' => 'MyApiKey',
- *   ));
+ * The $customer_id can now be used in a createCard() call.
  *
- *   // Create a credit card object
- *   // This card can be used for testing.
- *   // The CreditCard object is also used for creating customers.
- *   $card = new CreditCard(array(
- *               'firstName'    => 'Example',
- *               'lastName'     => 'Customer',
- *               'number'       => '4242424242424242',
- *               'expiryMonth'  => '01',
- *               'expiryYear'   => '2020',
- *               'cvv'          => '123',
- *               'email'                 => 'customer@example.com',
- *               'billingAddress1'       => '1 Scrubby Creek Road',
- *               'billingCountry'        => 'AU',
- *               'billingCity'           => 'Scrubby Creek',
- *               'billingPostcode'       => '4999',
- *               'billingState'          => 'QLD',
- *   ));
+ * #### Create Customer using Card Object
  *
- *   // Do a create customer transaction on the gateway
- *   $response = $gateway->createCustomer(array(
- *       'card'                     => $card,
- *   ))->send();
- *   if ($response->isSuccessful()) {
- *       echo "Gateway createCustomer was successful.\n";
- *       // Find the customer ID
- *       $customer_id = $response->getCustomerReference();
- *       echo "Customer ID = " . $customer_id . "\n";
- *       // Find the card ID
- *       $card_id = $response->getCardReference();
- *       echo "Card ID = " . $card_id . "\n";
- *   }
+ * Historically, this library used a card object to create customers.
+ * Although this is no longer the recommended path, it is still supported.
+ * Using this approach, a customer object and a card object can be created
+ * at the same time.
+ *
+ * <code>
+ * // Create a credit card object
+ * // This card can be used for testing.
+ * // The CreditCard object is also used for creating customers.
+ * $card = new CreditCard(array(
+ *             'firstName'    => 'Example',
+ *             'lastName'     => 'Customer',
+ *             'number'       => '4242424242424242',
+ *             'expiryMonth'  => '01',
+ *             'expiryYear'   => '2020',
+ *             'cvv'          => '123',
+ *             'email'                 => 'customer@example.com',
+ *             'billingAddress1'       => '1 Scrubby Creek Road',
+ *             'billingCountry'        => 'AU',
+ *             'billingCity'           => 'Scrubby Creek',
+ *             'billingPostcode'       => '4999',
+ *             'billingState'          => 'QLD',
+ * ));
+ *
+ * // Do a create customer transaction on the gateway
+ * $response = $gateway->createCustomer(array(
+ *     'card'                     => $card,
+ * ))->send();
+ * if ($response->isSuccessful()) {
+ *     echo "Gateway createCustomer was successful.\n";
+ *     // Find the customer ID
+ *     $customer_id = $response->getCustomerReference();
+ *     echo "Customer ID = " . $customer_id . "\n";
+ *     // Find the card ID
+ *     $card_id = $response->getCardReference();
+ *     echo "Card ID = " . $card_id . "\n";
+ * }
  * </code>
  *
  * @link https://stripe.com/docs/api#customers
