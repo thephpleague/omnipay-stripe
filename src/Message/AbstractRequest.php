a3f0b24721c0a9e024f109bad808b0c4bfc192eb
Andrew Coates <andycoatz@gmail.com>
2016-08-04 16:32:48 +0100

    Make sure Curl uses TLS >= v1.2. Fixes #53.

--- src/Message/AbstractRequest.php
+++ src/Message/AbstractRequest.php
@@ -126,6 +126,13 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
 
     public function sendData($data)
     {
+        // Stripe only accepts TLS >= v1.2, so make sure Curl is told
+        $config = $this->httpClient->getConfig();
+        $curlOptions = $config->get('curl.options');
+        $curlOptions[CURLOPT_SSLVERSION] = 6;
+        $config->set('curl.options', $curlOptions);
+        $this->httpClient->setConfig($config);
+        
         // don't throw exceptions for 4xx errors
         $this->httpClient->getEventDispatcher()->addListener(
             'request.error',
